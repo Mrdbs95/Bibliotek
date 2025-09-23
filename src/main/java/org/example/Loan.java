76fa1d0afd7d23f7e4dca43ec2dbd127a60e1d1b
alexandersson98 <alexandersson98l@gmail.com>
2025-09-23 09:21:17 +0200

    Create Loan.java

---
+++ src/main/java/org/example/Loan.java
@@ -0,0 +1,28 @@
+package org.example;
+
+
+    public class Loan {
+        private Book book;
+        private Member member;
+        private Library library;
+        private boolean avaiable;
+
+        public Loan(Book book, Member member,Library library) {
+            this.member = member;
+            this.book = book;
+            this.library = library;
+        }
+
+        public Book getBook(){return book;}
+        public Member getMember(){return member;}
+        public Library getLibrary(){return library;}
+
+        public void setAvaiable(){
+
+        }
+
+
+
+
+
+    }
