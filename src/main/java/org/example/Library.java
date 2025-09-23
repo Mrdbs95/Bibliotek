eefa9a91a8300088af6a5692a5f672824b905374
Daniel B <158176937+Mrdbs95@users.noreply.github.com>
2025-09-23 09:15:31 +0200

    Library class

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-    private List
+    private List <Book> books = new ArrayList<>();
+    private List <Member> members = new ArrayList<>();
+    private List <Loan> loan = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void registerMember(Member member) {
+        members.add(member);
+    }
 }
