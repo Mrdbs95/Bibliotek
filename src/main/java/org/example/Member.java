66c935976561e9d6430e1289f2cc4f34deb632f7
kh-rasha <rasha.alkhateeb97@gmail.com>
2025-09-23 10:08:43 +0200

    member

--- src/main/java/org/example/Member.java
+++ src/main/java/org/example/Member.java
@@ -5,7 +5,38 @@ public class Member {
     private String Name;
     private String email;
 
+
+
+    public String getId() { return id; }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
     public Member(String id, String name, String email){
         if (id == null || id .isBlank()) throw new IllegalArgumentException("id is required");
+        setName (name);
+        setEmail(email);
+        this .id = id.trim();
+
+
     }
+    @Override
+    public String toString(){
+        return "Member{id = '%s',email =' %s'}".formatted (id, name, email);
+    }
+
 }
