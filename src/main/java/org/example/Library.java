39286caaaad23ee1bd5a1cd9b5a23e674963ff27
Daniel B <158176937+Mrdbs95@users.noreply.github.com>
2025-09-23 10:09:15 +0200

    update library

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-    private List <Book> books = new ArrayList<>();
-    private List <Member> members = new ArrayList<>();
-    private List <Loan> loan = new ArrayList<>();
+    private List<Book> books = new ArrayList<>();
 
+    // Lägg till en bok
     public void addBook(Book book) {
         books.add(book);
     }
